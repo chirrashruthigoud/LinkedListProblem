9d0e2bf6562db5e835c98513be59af49d15b19c3
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-24 23:53:16 +0530

    Create Linked List

---
+++ LinkedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                }
+                temp.next = node;
+
+            }
+            Console.WriteLine("{0} insert into linked List ", node.data);
+
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+
+            }
+            Console.WriteLine("After storing in LinkedList value!!");
+            while (temp != null)
+            {
+
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+
