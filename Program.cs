5a2e7270570dc7f2e680a7969be6af23562e8134
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-24 23:46:15 +0530

    Welcome to Linked List Problem

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+
+namespace DataStructure
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Linked List Problem");
+           /* LinkedList linked = new LinkedList();
+            linked.add(56);
+            linked.add(30);
+            linked.add(70);
+            linked.Display();*/
+        }
+    }
+}
