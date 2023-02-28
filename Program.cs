802a0f398781eb7f6942a65ff4d832725bb60d0e
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-28 11:52:26 +0530

    Ability to delete 40 from LinkedList sequence of 56->30->40->70

--- Program.cs
+++ Program.cs
@@ -9,14 +9,12 @@ namespace DataStructure
         public static void Main(string[] args)
         {
             LinkedList linked = new LinkedList();
-            linked.Add(56); // add a elemt at last postion Append is nothinf act as Add () method
+            linked.Add(56); // add a elemt at last postion Append is nothing act as Add () method
             linked.Add(30);
+            linked.Add(40);
             linked.Add(70);
-            linked.Display();
-            int a = linked.Search(30);
-            Console.WriteLine("index position of element is :" + a);
-            linked.InsertAtParticularPosition(a + 1, 40);
-            linked.Display();
-        }
+            linked.del();
+        
+         }
     }
 }
